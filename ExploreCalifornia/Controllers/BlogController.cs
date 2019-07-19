50eda86efeef6aed3de9c12ae5803c809142d927
Gireshbabu Yemparala <A1830486@mail.niu.edu>
2019-07-19 17:30:34 -0500

    Add project files.

---
+++ ExploreCalifornia/Controllers/BlogController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExploreCalifornia.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return new ContentResult { Content = "Blog posts" };
+        }
+
+        [Route("blog/{year:int?}/{month:int?}/{key}")]
+        public IActionResult Post(int year, int month, string key)
+        {
+            return new ContentResult { Content = string.Format("Year: {0}; Month: {1}; Key: {2}", year, month, key) };
+        }
+    }
+}
---
+++ ExploreCalifornia/Startup.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ExploreCalifornia
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+        public Startup(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<FeatureToggles>(x => new FeatureToggles
+            {
+                DeveloperExceptions = _configuration.GetValue<bool>("FeatureToggles:DeveloperExceptions")
+            });
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, FeatureToggles features)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //app.Use(async (context, next) =>
+            //{
+            //    if(context.Request.Path.Value.StartsWith("/hello"))
+            //    {
+            //        await context.Response.WriteAsync("Hello World! ASP.NET Core");
+            //    }
+            //    await next();
+            //});
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+
+            app.UseExceptionHandler("/error.html");
+
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+
+            //if (_configuration["EnableDeveloperExceptions"] == "True")
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+
+            //if (_configuration.GetValue<bool>("FeatureToggles:DeveloperExceptions"))
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+
+
+            if (features.DeveloperExceptions)
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute("Default", "{controller=Home}/{action=Index}/{Id:int?}"
+
+                 );
+            });
+
+            app.UseFileServer();
+
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path.Value.StartsWith("/invalid"))
+                {
+                    throw new Exception("error");
+                }
+                await next();
+            });
+
+
+
+        }
+    }
+}

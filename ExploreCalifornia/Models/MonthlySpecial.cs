9538046f617553dab0483346b3207d81f2e1e209
Gireshbabu Yemparala <A1830486@mail.niu.edu>
2019-07-24 11:31:39 -0500

    enhanced ViewComponent

---
+++ ExploreCalifornia/Models/MonthlySpecial.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExploreCalifornia.Models
+{
+
+    public class MonthlySpecial
+    {
+        public string Key { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Price { get; set; }
+    }
+ 
+}

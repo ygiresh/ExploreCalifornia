26005fa9e300613a02d33bf89691f0b4832669d2
Gireshbabu Yemparala <A1830486@mail.niu.edu>
2019-07-24 11:16:21 -0500

    added basic ViewComponent

---
+++ ExploreCalifornia/ViewComponent/MonthlySpecialsViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExploreCalifornia.ViewComponent
+{
+    /// <summary>
+    ///  FOR A VIEW COMPONENT WE HAVE THREE OPTIONS
+    ///  1. Create class with View Componenet Name
+    ///  2. Decorate class with [ViewComponent]
+    ///  3. Inherit :ViewComponent 
+    ///  
+    /// In this example I named the class as ViewComponent (option 1)
+    /// </summary>
+    public class MonthlySpecialsViewComponent
+    {
+        public string Invoke()
+        {
+            return "TODO: MONTHLY HELPERS";
+        }
+
+    }
+}
